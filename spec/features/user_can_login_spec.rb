66ae84de430abc30fcfd5c12cc771349a15c09ba
Rich Shea <richcshea@gmail.com>
2015-03-07 13:23:00 -0700

    authenticate works

---
+++ spec/features/user_can_login_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe "A user" do
+  it "can login" do
+    user = User.create!(user_attributes)
+    visit '/'
+    click_link_or_button("Login")
+    expect(current_path).to eq(new_session_path)
+    fill_in "session[email]", with: user.email
+    fill_in "session[password]", with: user.password
+    click_link_or_button("Submit")
+    expect(page).to have_link("Logout")
+    expect(page).not_to have_link("Login")
+  end
+end
