8c767642a71b886dbfc7062bc6f5905eddbb6c27
Rich Shea <richcshea@gmail.com>
2015-03-08 13:14:54 -0600

    visit can create new account

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,24 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @ideas = @user.ideas 
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    user = User.new(user_params)
+    if user.save!
+      session[:user_id] = user.id
+      redirect_to user_path(user)
+    else
+      render :new
+    end
+  end
+
+private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
+  end
 end
